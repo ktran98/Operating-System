4783d6195e3168f266f5993eccf932b917434f27
ktran98 <peterkhanhtran@gmail.com>
2016-10-13 13:04:40 -0500

    Add files via upload
    almost finish

--- AppObject.js
+++ AppObject.js
@@ -1,39 +1,42 @@
-function App(n,m,s){
+function App(n, m, s){
 
     //Declare your instance variables here.
-    this.name = n;
-    this.memory = m;
-    this.state = s;
+    this.name = n
+    this.memory = m
+    this.state = s
 
     //Declare the instance function open here.
     this.open = function(){
-      this.state = active;
-
+      this.state = "active";
     };
+
     //Declare the instance function sleep here.
     this.sleep = function(){
-      If this.state = active;
-      this.state = sleep
-
-
+      if (this.state = "active");
+      this.state = "sleep";
+      this.memory = (this.memory)/2
     };
+
     //Declare the instance function active here.
     this.active = function(){
-
-
-
+      if (this.state = "sleep");
+      this.state = "active";
+      this.memory = (this.memory)*2
     };
+
     //Declare the instance function close here.
     this.close = function(){
-
-
-
-    }
+      if (this.state != "null");
+      this.state = "null";
+      this.memory = 0;
+    };
 }
 
 //Use the constructor to create 4 App objects below.
-
-
-
+var snapchat = new App (camera,5,active)
+var location = new App (Gps,6,sleep)
+var note = new App (diary,2,null)
+var game = new App (pokemon,4,sleep)
 //Declare an array named appList, and place all 4 of your apps in that array.
 //The order does not matter.
+this.appList = function()
